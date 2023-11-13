17f90c39edabb48191f924f4c441bf2240e62244
Atanu Shuvam Roy <atanuroy911@gmail.com>
2023-11-14 02:35:48 +0530

    audio test

--- audio.py
+++ audio.py
@@ -21,47 +21,47 @@ class audio():
             file_exists = ""
             for i in range(len(paths)):  # loop through creating the folders specified
                 try:
-                    os.mkdir(file_exists+paths[i])
-                    file_exists += paths[i]+"/"
+                    os.mkdir(file_exists + paths[i])
+                    file_exists += paths[i] + "/"
                 except:
-                    file_exists += paths[i]+"/"
+                    file_exists += paths[i] + "/"
         mic.threshold = 0
 
     def recordAudio(mic, time):
-        recording = sd.rec(int(time*mic.fs), samplerate=mic.fs, channels=1)
+        recording = sd.rec(int(time * mic.fs), samplerate=mic.fs, channels=1)
         sd.wait()
         return recording
 
     def save(mic, filename, recording):
-        wavio.write(mic.path+filename, recording, mic.fs, sampwidth=2)
+        wavio.write(mic.path + filename, recording, mic.fs, sampwidth=2)
 
     def play(mic, filename):
-        data, fs = sf.read(mic.path+filename, dtype='float32')
+        data, fs = sf.read(mic.path + filename, dtype='float32')
         sd.play(data, fs)
         status = sd.wait()
 
     def getText(mic, filename):  # use google speech recongition to convert
         r = sr.Recognizer()
-        hellow = sr.AudioFile(mic.path+filename)
+        hellow = sr.AudioFile(mic.path + filename)
         with hellow as source:
             audio = r.record(source)
         try:
             s = r.recognize_google(audio)
             return (s)
-        except:
-            print("error reading")
+        except Exception as e:
+            print("Error reading:", e)
         return ""
 
     def getSample(mic, seconds):
         num = []
 
         def print_sound(indata, outdata, frames, time, status):
-            volume_norm = np.linalg.norm(indata)*10
+            volume_norm = np.linalg.norm(indata) * 10
             # normalize data while adding
-            num.append(int(volume_norm)-mic.threshold)
+            num.append(int(volume_norm) - mic.threshold)
 
         with sd.Stream(callback=print_sound):
-            sd.sleep(500*seconds)
+            sd.sleep(500 * seconds)
         return num
 
     def displayLevels(mic, sample):  # display levels of sample
@@ -74,7 +74,7 @@ class audio():
         temp = (mic.getSample(3))
         for i in range(len(temp)):
             Sum += temp[i]
-        average = Sum/len(temp)
+        average = Sum / len(temp)
         mic.threshold = int(average)
         return mic.threshold  # return threshold
 
@@ -84,7 +84,7 @@ class audio():
             if sample[i] <= mic.threshold:
                 count += 1
         # if 75% of microphone low return true
-        return (count <= (len(sample)*0.75))
+        return (count <= (len(sample) * 0.75))
 
     def getVolume(mic, arr):
         largest = 0
@@ -97,19 +97,19 @@ class audio():
         infiles = files[0:2]
         data = []
         for infile in infiles:
-            w = wave.open(mic.path+infile, 'rb')
+            w = wave.open(mic.path + infile, 'rb')
             data.append([w.getparams(), w.readframes(w.getnframes())])
             w.close()
 
-        output = wave.open(mic.path+outfile, 'wb')
+        output = wave.open(mic.path + outfile, 'wb')
         output.setparams(data[0][0])
         output.writeframes(data[0][1])
         output.writeframes(data[1][1])
         output.close()
         files.remove(infiles[0])
         files.remove(infiles[1])
-        if len(files) > 0:  # recursivly merge all files
-            newAr = [outfile]+files  # create new array with all to add
+        if len(files) > 0:  # recursively merge all files
+            newAr = [outfile] + files  # create a new array with all to add
             mic.merge(newAr, outfile)
 
     def recordWhileActive(mic):
@@ -119,7 +119,7 @@ class audio():
         while sound:  # loop while sound is present
             a = None
             a = mic.recordAudio(3)  # run record concurrently
-            b = mic.getSample(1)  # run sound checker concurrent
+            b = mic.getSample(1)  # run sound checker concurrently
             b = mic.getVolume(b)
             print(b)
             if b <= mic.threshold:  # no more audio
@@ -128,16 +128,16 @@ class audio():
         print("end")
         infiles = []
         for i in range(len(sounds)):
-            mic.save(str(i)+".wav", sounds[i])
-            infiles.append(str(i)+".wav")
+            mic.save(str(i) + ".wav", sounds[i])
+            infiles.append(str(i) + ".wav")
         if len(infiles) > 1:
             mic.merge(infiles, "sounds.wav")
         else:
             # save file as sounds.wav
-            w = wave.open(mic.path+"0.wav", 'rb')
+            w = wave.open(mic.path + "0.wav", 'rb')
             data = ([w.getparams(), w.readframes(w.getnframes())])
             w.close()
-            output = wave.open(mic.path+"sounds.wav", 'wb')
+            output = wave.open(mic.path + "sounds.wav", 'wb')
             output.setparams(data[0])
             output.writeframes(data[1])
             output.close()
