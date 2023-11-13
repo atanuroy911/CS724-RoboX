d1440c48bee1887904d84c259d414cc8e4adcfc5
Atanu Shuvam Roy <atanuroy911@gmail.com>
2023-11-14 02:52:50 +0530

    audio test

--- audio.py
+++ audio.py
@@ -11,159 +11,177 @@ import os
 import sys
 # from concurrent.futures import ThreadPoolExecutor
 
+from ctypes import *
+from contextlib import contextmanager
+
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+
+def py_error_handler(filename, line, function, err, fmt):
+    pass
+
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+
+@contextmanager
+def noalsaerr():
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
+    yield
+    asound.snd_lib_error_set_handler(None)
+
 
 class audio():
-    def __init__(mic, path):
-        mic.fs = 44100
-        mic.path = path
-        if not (os.path.isdir(path)):  # make a pathway
-            paths = path.split("/")
-            file_exists = ""
-            for i in range(len(paths)):  # loop through creating the folders specified
-                try:
-                    os.mkdir(file_exists + paths[i])
-                    file_exists += paths[i] + "/"
-                except:
-                    file_exists += paths[i] + "/"
-        mic.threshold = 0
+    with noalsaerr():
+        def __init__(mic, path):
+            mic.fs = 44100
+            mic.path = path
+            if not (os.path.isdir(path)):  # make a pathway
+                paths = path.split("/")
+                file_exists = ""
+                for i in range(len(paths)):  # loop through creating the folders specified
+                    try:
+                        os.mkdir(file_exists + paths[i])
+                        file_exists += paths[i] + "/"
+                    except:
+                        file_exists += paths[i] + "/"
+            mic.threshold = 0
 
-    def recordAudio(mic, time):
-        recording = sd.rec(int(time * mic.fs), samplerate=mic.fs, channels=1)
-        sd.wait()
-        return recording
+        def recordAudio(mic, time):
+            recording = sd.rec(int(time * mic.fs), samplerate=mic.fs, channels=1)
+            sd.wait()
+            return recording
 
-    def save(mic, filename, recording):
-        wavio.write(mic.path + filename, recording, mic.fs, sampwidth=2)
+        def save(mic, filename, recording):
+            wavio.write(mic.path + filename, recording, mic.fs, sampwidth=2)
 
-    def play(mic, filename):
-        data, fs = sf.read(mic.path + filename, dtype='float32')
-        sd.play(data, fs)
-        status = sd.wait()
+        def play(mic, filename):
+            data, fs = sf.read(mic.path + filename, dtype='float32')
+            sd.play(data, fs)
+            status = sd.wait()
 
-    def getText(mic, filename):  # use google speech recongition to convert
-        r = sr.Recognizer()
-        hellow = sr.AudioFile(mic.path + filename)
-        with hellow as source:
-            audio = r.record(source)
-        try:
-            s = r.recognize_google(audio)
-            return (s)
-        except Exception as e:
-            print("Error reading:", e)
-        return ""
+        def getText(mic, filename):  # use google speech recongition to convert
+            r = sr.Recognizer()
+            hellow = sr.AudioFile(mic.path + filename)
+            with hellow as source:
+                audio = r.record(source)
+            try:
+                s = r.recognize_google(audio)
+                return (s)
+            except Exception as e:
+                print("Error reading:", e)
+            return ""
 
-    def getSample(mic, seconds):
-        num = []
+        def getSample(mic, seconds):
+            num = []
 
-        def print_sound(indata, outdata, frames, time, status):
-            volume_norm = np.linalg.norm(indata) * 10
-            # normalize data while adding
-            num.append(int(volume_norm) - mic.threshold)
+            def print_sound(indata, outdata, frames, time, status):
+                volume_norm = np.linalg.norm(indata) * 10
+                # normalize data while adding
+                num.append(int(volume_norm) - mic.threshold)
 
-        with sd.Stream(callback=print_sound):
-            sd.sleep(500 * seconds)
-        return num
+            with sd.Stream(callback=print_sound):
+                sd.sleep(500 * seconds)
+            return num
 
-    def displayLevels(mic, sample):  # display levels of sample
-        for i in range(len(sample)):
-            print("|" * sample[i])
+        def displayLevels(mic, sample):  # display levels of sample
+            for i in range(len(sample)):
+                print("|" * sample[i])
 
-    def getThreshold(mic):  # set and show threshold
-        print("A moment of silence")
-        Sum = 0
-        temp = (mic.getSample(3))
-        for i in range(len(temp)):
-            Sum += temp[i]
-        average = Sum / len(temp)
-        mic.threshold = int(average)
-        return mic.threshold  # return threshold
+        def getThreshold(mic):  # set and show threshold
+            print("A moment of silence")
+            Sum = 0
+            temp = (mic.getSample(3))
+            for i in range(len(temp)):
+                Sum += temp[i]
+            average = Sum / len(temp)
+            mic.threshold = int(average)
+            return mic.threshold  # return threshold
 
-    def ready(mic, sample):  # show readiness
-        count = 0
-        for i in range(len(sample)):
-            if sample[i] <= mic.threshold:
-                count += 1
-        # if 75% of microphone low return true
-        return (count <= (len(sample) * 0.75))
+        def ready(mic, sample):  # show readiness
+            count = 0
+            for i in range(len(sample)):
+                if sample[i] <= mic.threshold:
+                    count += 1
+            # if 75% of microphone low return true
+            return (count <= (len(sample) * 0.75))
 
-    def getVolume(mic, arr):
-        largest = 0
-        for i in range(len(arr)):
-            if arr[i] > largest:
-                largest = arr[i]
-        return largest
+        def getVolume(mic, arr):
+            largest = 0
+            for i in range(len(arr)):
+                if arr[i] > largest:
+                    largest = arr[i]
+            return largest
 
-    def merge(mic, files, outfile):
-        infiles = files[0:2]
-        data = []
-        for infile in infiles:
-            w = wave.open(mic.path + infile, 'rb')
-            data.append([w.getparams(), w.readframes(w.getnframes())])
-            w.close()
+        def merge(mic, files, outfile):
+            infiles = files[0:2]
+            data = []
+            for infile in infiles:
+                w = wave.open(mic.path + infile, 'rb')
+                data.append([w.getparams(), w.readframes(w.getnframes())])
+                w.close()
 
-        output = wave.open(mic.path + outfile, 'wb')
-        output.setparams(data[0][0])
-        output.writeframes(data[0][1])
-        output.writeframes(data[1][1])
-        output.close()
-        files.remove(infiles[0])
-        files.remove(infiles[1])
-        if len(files) > 0:  # recursively merge all files
-            newAr = [outfile] + files  # create a new array with all to add
-            mic.merge(newAr, outfile)
-
-    def recordWhileActive(mic):
-        sound = True
-        sounds = []
-        print("begin")
-        while sound:  # loop while sound is present
-            a = None
-            a = mic.recordAudio(3)  # run record concurrently
-            b = mic.getSample(1)  # run sound checker concurrently
-            b = mic.getVolume(b)
-            print(b)
-            if b <= mic.threshold:  # no more audio
-                sound = False
-            sounds.append(a)
-        print("end")
-        infiles = []
-        for i in range(len(sounds)):
-            mic.save(str(i) + ".wav", sounds[i])
-            infiles.append(str(i) + ".wav")
-        if len(infiles) > 1:
-            mic.merge(infiles, "sounds.wav")
-        else:
-            # save file as sounds.wav
-            w = wave.open(mic.path + "0.wav", 'rb')
-            data = ([w.getparams(), w.readframes(w.getnframes())])
-            w.close()
-            output = wave.open(mic.path + "sounds.wav", 'wb')
-            output.setparams(data[0])
-            output.writeframes(data[1])
+            output = wave.open(mic.path + outfile, 'wb')
+            output.setparams(data[0][0])
+            output.writeframes(data[0][1])
+            output.writeframes(data[1][1])
             output.close()
+            files.remove(infiles[0])
+            files.remove(infiles[1])
+            if len(files) > 0:  # recursively merge all files
+                newAr = [outfile] + files  # create a new array with all to add
+                mic.merge(newAr, outfile)
 
-    def readFrequencies(mic, filename):
-        spf = wave.open(filename, "r")
+        def recordWhileActive(mic):
+            sound = True
+            sounds = []
+            print("begin")
+            while sound:  # loop while sound is present
+                a = None
+                a = mic.recordAudio(3)  # run record concurrently
+                b = mic.getSample(1)  # run sound checker concurrently
+                b = mic.getVolume(b)
+                print(b)
+                if b <= mic.threshold:  # no more audio
+                    sound = False
+                sounds.append(a)
+            print("end")
+            infiles = []
+            for i in range(len(sounds)):
+                mic.save(str(i) + ".wav", sounds[i])
+                infiles.append(str(i) + ".wav")
+            if len(infiles) > 1:
+                mic.merge(infiles, "sounds.wav")
+            else:
+                # save file as sounds.wav
+                w = wave.open(mic.path + "0.wav", 'rb')
+                data = ([w.getparams(), w.readframes(w.getnframes())])
+                w.close()
+                output = wave.open(mic.path + "sounds.wav", 'wb')
+                output.setparams(data[0])
+                output.writeframes(data[1])
+                output.close()
 
-        # Extract Raw Audio from Wav File
-        signal = spf.readframes(-1)
-        signal = np.fromstring(signal, "Int16")
-        arr = []
-        return signal
+        def readFrequencies(mic, filename):
+            spf = wave.open(filename, "r")
 
-    def plotFile(mic, filename):
-        spf = wave.open(filename, "r")
+            # Extract Raw Audio from Wav File
+            signal = spf.readframes(-1)
+            signal = np.fromstring(signal, "Int16")
+            arr = []
+            return signal
 
-        # Extract Raw Audio from Wav File
+        def plotFile(mic, filename):
+            spf = wave.open(filename, "r")
 
-        signal = spf.readframes(-1)
-        signal = np.fromstring(signal, "Int16")
+            # Extract Raw Audio from Wav File
 
-        if spf.getnchannels() == 2:
-            print("Just mono files")
-            sys.exit(0)
+            signal = spf.readframes(-1)
+            signal = np.fromstring(signal, "Int16")
 
-        plt.figure(1)
-        plt.title("Signal Wave...")
-        plt.plot(signal)
-        plt.show()
+            if spf.getnchannels() == 2:
+                print("Just mono files")
+                sys.exit(0)
+
+            plt.figure(1)
+            plt.title("Signal Wave...")
+            plt.plot(signal)
+            plt.show()
