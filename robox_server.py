df64dfe7b8aaf9d86626a8fdc5f89273afee3896
Atanu Shuvam Roy <atanuroy911@gmail.com>
2023-11-16 21:56:37 +0530

    ultra

--- robox_server.py
+++ robox_server.py
@@ -10,6 +10,14 @@ from chatBot import predictAnswer
 import threading
 import time
 
+# set GPIO Pins
+GPIO_TRIGGER = 2
+GPIO_ECHO = 3
+
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
 # Define GPIO pins
 IN1 = 17
 IN2 = 27
@@ -49,6 +57,65 @@ print("Initilization of Robo assistant x is completed")
 
 # Use a lock to control access to the prompt function
 prompt_lock = threading.Lock()
+
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
+def check_distance_routine():
+    initial_speed = 50
+    try:
+        while True:
+            dist = distance()
+            print("Measured Distance = %.1f cm" % dist)
+
+            if dist < 30:
+                stop()
+                time.sleep(1)  # Pause for 1 second
+
+                # Change direction and move backward
+                move_backward(initial_speed)
+                time.sleep(1)  # Pause for 1 second
+
+                # Change direction and turn right (you can customize the direction)
+                turn_right(initial_speed)
+                time.sleep(1)  # Pause for 1 second
+
+                # Change direction back to forward
+                move_forward(initial_speed)
+
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
+
+@app.before_first_request
+def activate_job():
+    thread = threading.Thread(target=check_distance_routine)
+    thread.start()
 
 @app.route('/check_connection')
 def check_connection():
